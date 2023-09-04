db1179203573ead1926c80104b3482504511e4cd
Airi Nakama <62741497+milky-airi@users.noreply.github.com>
2023-09-04 06:23:29 +0000

    Ratyの導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,10 @@ import "jquery";
 import "popper.js";
 import "bootstrap";
 import "../stylesheets/application";
+
+import Raty from "raty.js"
+window.raty = function(elem,opt) {
+ let raty =  new Raty(elem,opt)
+ raty.init();
+ return raty;
+}
