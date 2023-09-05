b4f8979e66b8c5fcb56ee7518376419cd12c50c8
Airi Nakama <62741497+milky-airi@users.noreply.github.com>
2023-09-05 14:18:46 +0000

    管理者側条件検索機能追加

--- app/controllers/admin/detail_searches_controller.rb
+++ app/controllers/admin/detail_searches_controller.rb
@@ -1,4 +1,12 @@
 class Admin::DetailSearchesController < ApplicationController
+
   def detail_search
+    @prefecture = params[:prefecture]
+    @nationalities = params[:nationality]&.split(' ')
+    @anual_fee_greater_than_or_equal = params[:anual_fee_greater_than_or_equal]
+    @anual_fee_less_than = params[:anual_fee_less_than]
+    @word = params[:word]
+    @schools = School.look_detail_for(@prefecture, @nationalities, @anual_fee_greater_than_or_equal, @anual_fee_less_than, @word)
   end
+
 end
--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,4 +1,5 @@
 class Admin::HomesController < ApplicationController
   def top
+    @schools = School.where(is_open: true)
   end
 end
