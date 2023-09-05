04934f1e03053039caa27bf7ab07379b305ff58f
Airi Nakama <62741497+milky-airi@users.noreply.github.com>
2023-09-05 07:54:57 +0000

    管理者側通報機能

--- app/controllers/admin/alerts_controller.rb
+++ app/controllers/admin/alerts_controller.rb
@@ -8,9 +8,19 @@ class Admin::AlertsController < ApplicationController
   end
 
   def edit
+    @alert = Alert.find(params[:id])
   end
 
   def update
+    alert = Alert.find(params[:id])
+    alert.update(alert_params)
+    redirect_to admin_alerts_path
   end
+
+  private
+
+    def alert_params
+      params.require(:alert).permit(:review_id, :user_id, :reason, :is_done)
+    end
 
 end
