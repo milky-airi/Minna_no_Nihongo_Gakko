857ba054c983de4fff7233db29334e828e1670db
Airi Nakama <62741497+milky-airi@users.noreply.github.com>
2023-09-05 12:12:07 +0000

    退会済みユーザーのログインを阻止

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
+  before_action :is_deleted?, only: [:create]
 
   def guest_sign_in
     user = User.guest
@@ -8,5 +9,20 @@ class Public::SessionsController < Devise::SessionsController
     flash.now[:notice] = "ゲストユーザーとしてログインしました。"
     redirect_to root_path
   end
+
+  protected
+
+    def is_deleted?
+      @user = User.find_by(email: params[:user][:email])
+      return if !@user
+      if @user.valid_password?(params[:user][:password])
+        if @user.is_deleted == true
+          flash[:notice] = "このアカウントは退会済みです。新規登録してください。"
+        redirect_to new_user_registration_path
+        else
+          return
+        end
+      end
+    end
 
 end
