6dd8be947304eb6a6afce19da6743462365590cf
Airi Nakama <62741497+milky-airi@users.noreply.github.com>
2023-09-05 10:36:09 +0000

    ゲストログイン機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,20 @@ class User < ApplicationRecord
   has_one :went_school
   has_many :reviews, dependent: :destroy
 
-
   def country
     Carmen::Country.coded(country_code)
+  end
+
+  # ゲストログイン用データ
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "ゲスト"
+      user.name_kana = "げすと"
+      user.country_code = "JP"
+      user.job = "大学生"
+      user.is_deleted = false
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
 
-  namespace :public do
-    get 'went_schools/new'
-    get 'went_schools/edit'
-  end
   devise_for :users,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
@@ -12,6 +8,11 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
   }
+
+  # ユーザーログイン
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'public/sessions#guest_sign_in'
+  end
 
   # 管理者側
   namespace :admin do
