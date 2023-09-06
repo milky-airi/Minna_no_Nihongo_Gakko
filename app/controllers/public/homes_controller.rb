9db4fbdf5197b70d0ca420a7e76dfa0c588e4c4d
Airi Nakama <62741497+milky-airi@users.noreply.github.com>
2023-09-06 07:00:44 +0000

    ユーザー側トップ画面のレイアウト調整

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,5 +2,6 @@ class Public::HomesController < ApplicationController
   def top
     @schools = School.where(is_open: true)
     @top_review_schools = School.where(is_open: true).order(params[:sort]).limit(4)
+    @reviews = Review.all.last(4)
   end
 end
