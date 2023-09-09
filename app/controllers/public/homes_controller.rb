d6daf464176b4cbf382bc6bb313c4473f68a013b
Airi Nakama <62741497+milky-airi@users.noreply.github.com>
2023-09-09 03:27:45 +0000

    レビュー詳細画面レイアウト調整

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,6 +2,6 @@ class Public::HomesController < ApplicationController
   def top
     @schools = School.where(is_open: true)
     @top_review_schools = School.where(is_open: true).order(params[:sort]).limit(4)
-    @reviews = Review.all.last(4)
+    @reviews = Review.where(is_open: true).last(4)
   end
 end
