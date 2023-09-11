c92159b1f9404b4d3416a3b05379ecde448b9e38
Airi Nakama <62741497+milky-airi@users.noreply.github.com>
2023-09-11 16:48:01 +0000

    レビューいいねの通知機能追加

--- app/controllers/public/nice_reviews_controller.rb
+++ app/controllers/public/nice_reviews_controller.rb
@@ -5,6 +5,7 @@ class Public::NiceReviewsController < ApplicationController
     @review = Review.find(params[:review_id])
     nice_review = current_user.nice_reviews.new(review_id: @review.id)
     nice_review.save
+    @review.create_nice_notification_by(current_user)
     # redirect_back(fallback_location: root_path)
   end
 
--- app/models/review.rb
+++ app/models/review.rb
@@ -17,7 +17,7 @@ class Review < ApplicationRecord
   validates :evaluation_life, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 
 
-  # 通知作成
+  # コメント通知作成
   def create_notification_by(current_user)
       notification = current_user.active_notifications.new(
         review_id: id,
@@ -25,15 +25,31 @@ class Review < ApplicationRecord
         action: "comment"
       )
 
-  if notification.visiter_id == notification.visited_id
-     notification.is_checked = true
-  end
+    if notification.visiter_id == notification.visited_id
+       notification.is_checked = true
+    end
 
     notification.save if notification.valid?
   end
 
+  # レビューいいね済みか
   def niced_by?(user)
 	  nice_reviews.exists?(user_id: user.id)
+  end
+
+  # レビューいいね通知作成
+  def create_nice_notification_by(current_user)
+      notification = current_user.active_notifications.new(
+        review_id: id,
+        visited_id: user_id,
+        action: "nice"
+      )
+
+    if notification.visiter_id == notification.visited_id
+       notification.is_checked = true
+    end
+
+    notification.save if notification.valid?
   end
 
 
