dacda65b3e732f797357a0abc44e47c8ed4aeae1
Airi Nakama <62741497+milky-airi@users.noreply.github.com>
2023-09-11 13:44:25 +0000

    ログイン後にログイン前の画面に遷移する

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  after_action :store_location
+
+  def store_location
+   # 今回の場合は、 /users/sign_in , /users/sign_up, /users/password にアクセスしたとき、ajaxでのやりとりはsessionには保存しない。
+      if (request.fullpath != "/users/sign_in" && \
+          request.fullpath != "/users/sign_up" && \
+          request.fullpath != "/users/password" && \
+          !request.xhr?) # don't store ajax calls
+        session[:previous_url] = request.fullpath
+      end
+  end
+
+  #ログイン後のリダイレクトをログイン前のページにする
+  def after_sign_in_path_for(resource)
+    session[:previous_url] || root_path
+  end
+
 end
