c5977eea51928dd2390f50eccd1f243745d58212
Airi Nakama <62741497+milky-airi@users.noreply.github.com>
2023-09-13 07:08:23 +0000

    マイグレーションファイルの修正

--- db/migrate/20230904021353_remove_column_from_user.rb
+++ db/migrate/20230904021353_remove_column_from_user.rb
@@ -1,6 +1,6 @@
 class RemoveColumnFromUser < ActiveRecord::Migration[6.1]
   def change
-    remove_column :users, :school_id, :integer
+    # remove_column :users, :school_id, :integer
     remove_column :users, :graduated_at, :integer
   end
 end
