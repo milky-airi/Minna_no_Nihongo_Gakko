0211ec7fe4d68b9f9a7e9121086e4913e829ebe0
Airi Nakama <62741497+milky-airi@users.noreply.github.com>
2023-09-13 00:57:53 +0000

    ページネーション追加・出身校登録レイアウト調整

--- app/controllers/public/schools_controller.rb
+++ app/controllers/public/schools_controller.rb
@@ -3,7 +3,7 @@ class Public::SchoolsController < ApplicationController
   def show
     @school = School.find(params[:id])
     @courses = @school.courses
-    @reviews = @school.reviews.where(is_open: true)
+    @reviews = @school.reviews.where(is_open: true).page(params[:page]).per(10).order(created_at: :desc)
 
   end
 
