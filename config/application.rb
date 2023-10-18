8c47b538f22795fb96c7c798096f972edc6cefe0
Airi Nakama <62741497+milky-airi@users.noreply.github.com>
2023-10-18 09:05:25 +0000

    Update timezone

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,8 @@ module NihongoGakko
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = 'Tokyo'
+
     # config.eager_load_paths << Rails.root.join("extras")
     config.i18n.default_locale = :ja
   end
