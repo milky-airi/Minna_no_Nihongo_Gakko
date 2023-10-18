c8a0c01187aac04f050ff0b3ef34e9baadefe4dd
Airi Nakama <62741497+milky-airi@users.noreply.github.com>
2023-10-18 08:58:38 +0000

    Add ensure_correct_user

--- app/controllers/public/reviews_controller.rb
+++ app/controllers/public/reviews_controller.rb
@@ -1,5 +1,6 @@
 class Public::ReviewsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy, :indeividual]
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
 
   def new
     @review = Review.new
@@ -60,6 +61,14 @@ class Public::ReviewsController < ApplicationController
 
     def review_params
       params.require(:review).permit(:user_id, :school_id, :star_integer, :comment_integer, :evaluation_class, :comment_class, :evaluation_after_graduation, :comment_after_graduation, :evaluation_place, :comment_place, :evaluation_facility, :comment_facility, :evaluation_student, :comment_student, :evaluation_teacher, :comment_teacher, :evaluation_life, :comment_life, :is_open)
+    end
+
+    def ensure_correct_user
+      @review = Review.find(params[:id])
+      unless @review.user_id == current_user.id
+        flash[:notice] = "アクセス権限がありません"
+        redirect_to root_path
+      end
     end
 
 end
