5d9c494b2d53c4fc810ad89147e56045052bed0d
Airi Nakama <62741497+milky-airi@users.noreply.github.com>
2023-10-18 08:34:05 +0000

    Update school/show

--- app/controllers/public/schools_controller.rb
+++ app/controllers/public/schools_controller.rb
@@ -4,6 +4,11 @@ class Public::SchoolsController < ApplicationController
     @school = School.find(params[:id])
     @courses = @school.courses
     @reviews = @school.reviews.where(is_open: true).page(params[:page]).per(10).order(created_at: :desc)
+
+    # 非公開のスクールの場合、エラーメッセージを表示してリダイレクト
+    unless @school.is_open
+      redirect_to root_path and return
+    end
   end
 
 end
